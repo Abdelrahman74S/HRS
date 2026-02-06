a1bb88a6bb10902ecf715ef11f6e33215777af74
Abdelrahman74S <bdalrhmnr927@gmail.com>
2026-02-06 21:15:28 +0200

    Version 4.0  final

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,7 +5,7 @@ from .views import (
     RoomUpdateView,
     RoomDetailView,
     RoomDeleteView,
-    search,
+    # search,
 )
 
 app_name ='rooms'
@@ -15,7 +15,7 @@ urlpatterns = [
 
     path('create/', RoomCreateView.as_view(), name='room_create'),
 
-    path('search/', search, name='room_search'),
+    # path('search/', search, name='room_search'),
     path('<slug:slug>/', RoomDetailView.as_view(), name='room_detail'),
 
     path('<slug:slug>/update/', RoomUpdateView.as_view(), name='room_update'),
--- rooms/views.py
+++ rooms/views.py
@@ -6,6 +6,8 @@ from .forms import RoomForm, RoomImageFormSet
 from rooms.Ispermissions import IsManager
 from django.db.models import Q
 from django.shortcuts import render
+from datetime import datetime
+from Reservation.models import Booking
 
 class RoomCreateView(IsManager, CreateView):
     model = Room
@@ -65,8 +67,45 @@ class RoomListView(ListView):
     model = Room
     template_name = 'rooms/room_list.html'
     context_object_name = 'rooms'
-    paginate_by = 10 
+    paginate_by = 6
 
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(is_active=True)
+        
+        q = self.request.GET.get('q')
+        check_in = self.request.GET.get('check_in')
+        check_out = self.request.GET.get('check_out')
+
+        if q:
+            queryset = queryset.filter(
+                Q(room_number__icontains=q) |
+                Q(room_type__name__icontains=q) |
+                Q(notes__icontains=q)
+            )
+
+        if check_in and check_out:
+            try:
+                c_in = datetime.strptime(check_in, "%Y-%m-%d").date()
+                c_out = datetime.strptime(check_out, "%Y-%m-%d").date()
+                
+                booked_rooms_ids = Booking.objects.filter(
+                    status__in=['confirmed', 'checked_in', 'pending'],
+                    check_in_date__lt=c_out,
+                    check_out_date__gt=c_in
+                ).values_list('room_id', flat=True)
+                
+                queryset = queryset.exclude(id__in=booked_rooms_ids)
+            except ValueError:
+                pass 
+                
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q', '')
+        context['check_in'] = self.request.GET.get('check_in', '')
+        context['check_out'] = self.request.GET.get('check_out', '')
+        return context
 
 class RoomDetailView(DetailView):
     model = Room
@@ -82,20 +121,38 @@ class RoomDeleteView(IsManager, DeleteView):
     slug_url_kwarg = 'slug'
 
 
-def search(request):
-    q = request.GET.get('q', '')
-    if q:
-        rooms = Room.objects.filter(
-            Q(room_number__icontains=q) | 
-            Q(room_type__name__icontains=q) | 
-            Q(notes__icontains=q)
-        ).distinct()
-    else:
-        # rooms = Room.objects.all()
-        rooms = Room.objects.none()
+# def search(request):
+#     q = request.GET.get('q', '')
+#     check_in = request.GET.get('check_in')
+#     check_out = request.GET.get('check_out')
 
-    return render(request, 'rooms/search.html', {
-    # return render(request, 'rooms/room_list.html', {
-        'rooms': rooms,
-        'query': q
-    })
+#     rooms = Room.objects.filter(is_active=True)
+
+#     if q:
+#         rooms = rooms.filter(
+#             Q(room_number__icontains=q) |
+#             Q(room_type__name__icontains=q) |
+#             Q(notes__icontains=q)
+#         )
+
+#     if check_in and check_out:
+#         try:
+#             check_in_date = datetime.strptime(check_in, "%Y-%m-%d").date()
+#             check_out_date = datetime.strptime(check_out, "%Y-%m-%d").date()
+
+#             booked_rooms_ids = Booking.objects.filter(
+#                 status__in=['confirmed', 'checked_in', 'pending'],
+#                 check_in_date__lt=check_out_date,
+#                 check_out_date__gt=check_in_date
+#             ).values_list('room_id', flat=True)
+
+#             rooms = rooms.exclude(id__in=booked_rooms_ids)
+#         except ValueError:
+#             pass
+
+#     return render(request, 'rooms/search.html', {
+#         'rooms': rooms,
+#         'q': q,
+#         'check_in': check_in,
+#         'check_out': check_out
+#     })
